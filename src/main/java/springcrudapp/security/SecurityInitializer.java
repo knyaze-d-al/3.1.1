58034edaa44ee69333c8ae3443508755afe14371
knyaze-d-al <knyazev.d.al@gmail.com>
2020-10-11 21:35:04 +0300

    Commit1

--- src/main/java/springcrudapp/security/SecurityInitializer.java
+++
@@ -1,8 +0,0 @@
-package springcrudapp.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
